31bf3188935679676b5637c5a176eccc42d53b49
Andrew Youngwerth <drew.youngwerth@gmail.com>
2015-05-19 16:39:56 -0600

    Updated git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Incredibly simple datetimepicker',
   // URL to the Git repository containing the source code for this package.
-  git: 'git@github.com:drew-y/meteor-autoform-datetimepicker.git',
+  git: 'https://github.com/drew-y/meteor-autoform-datetimepicker',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
