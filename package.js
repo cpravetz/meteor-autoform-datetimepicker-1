c13006bc5a2561b75e0b620db3d6be2408b46f40
Pietro <pietro.abate@inria.fr>
2018-01-25 15:33:43 +0100

    Update description

--- package.js
+++ package.js
@@ -1,12 +1,8 @@
 Package.describe({
   name: 'abate:autoform-datetimepicker',
   version: '1.0.8',
-  // Brief, one-line summary of the package.
   summary: 'Incredibly simple datetimepicker',
-  // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/abate/meteor-autoform-datetimepicker',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
