1760790a54e93876ca69fc98caf216991a267e1c
Pietro <pietro.abate@inria.fr>
2018-02-21 22:54:00 +0100

    upgrade autoform 6.3.0

--- package.js
+++ package.js
@@ -10,7 +10,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
 
   api.use([
-    'aldeed:autoform@6.2.0',
+    'aldeed:autoform@6.0.0',
     'underscore',
     'templating',
     'blaze',
